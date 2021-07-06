c8869db66fb6373bbd27befa03b2b5a8578c6095
Vasile Pește <vasile.peste@protonmail.ch>
2021-07-06 14:05:00 +0200

    Add work in progress

--- src/core/indicators/MidaIndicator.ts
+++ src/core/indicators/MidaIndicator.ts
@@ -1,13 +1,21 @@
 import { MidaError } from "#errors/MidaError";
+import { MidaIndicatorParameters } from "#indicators/MidaIndicatorParameters";
+import { GenericObject } from "#utilities/GenericObject";
 
 export abstract class MidaIndicator {
-    protected constructor () {
-        // Silence is golden.
+    readonly #name: string;
+
+    protected constructor ({ name, }: MidaIndicatorParameters) {
+        this.#name = name;
     }
 
-    public abstract calc (...parameters: any[]): Promise<any>;
+    public get name (): string {
+        return this.#name;
+    }
 
-    public abstract next (...parameters: any[]): Promise<any>;
+    public abstract calculate (parameters: GenericObject): Promise<any>;
+
+    // public abstract next (parameters: GenericObject): Promise<any>;
 
     static readonly #installedIndicators: Map<string, typeof MidaIndicator> = new Map();
 
@@ -24,7 +32,7 @@ export abstract class MidaIndicator {
         MidaIndicator.#installedIndicators.set(name, indicatorConstructor);
     }
 
-    public static create (name: string): MidaIndicator {
+    public static create (name: string, parameters?: GenericObject): MidaIndicator {
         const indicatorConstructor: any | undefined = MidaIndicator.#installedIndicators.get(name);
 
         if (!indicatorConstructor) {
@@ -32,6 +40,6 @@ export abstract class MidaIndicator {
             throw new MidaError();
         }
 
-        return new indicatorConstructor();
+        return new indicatorConstructor(parameters);
     }
 }
